2ac8be90b08de6a17ba35048ba0d94ae230d8178
Sélèna Boullée <sboullee@kalya-sante.com>
2024-06-02 20:47:22 +0200

    fix : fix error geojson and tsconfig

--- src/app/components/WorldMap.tsx
+++ src/app/components/WorldMap.tsx
@@ -1,59 +1,65 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+// src/components/WorldMap.tsx
+import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L, { GeoJSON, LeafletMouseEvent } from 'leaflet';
 
 const WorldMap: React.FC = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json");
-        const geoJsonData = await response.json();
+  const mapRef = useRef<L.Map | null>(null);
 
-        const map = L.map('map').setView([30.505, -0.09], 2);
+  useEffect(() => {
+    const initializeMap = () => {
+      if (mapRef.current === null) {
+        mapRef.current = L.map('map').setView([30.505, -0.09], 2);
         L.tileLayer(
           'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
           }
-        ).addTo(map);
-        
-        const geoJsonLayer: GeoJSON = L.geoJSON(geoJsonData, {
-          style: function (feature) {
-            return { color: "white", weight: 1, fillColor: "white", fillOpacity: 0.4 };
-          },
-          onEachFeature: function (feature, layer: GeoJSON) {
-            layer.on('click', function (e: LeafletMouseEvent) {
-              const currentStyle = (layer.options as any).fillColor;
-              if (currentStyle === "white") {
-                layer.setStyle({ fillColor: "#2ABF7A", fillOpacity: 0.6 });
-              } else {
-                layer.setStyle({ fillColor: "white", fillOpacity: 0.4 });
-              }
-            });
-          }
-        }).addTo(map);
+        ).addTo(mapRef.current);
+      }
+    };
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json");
+        const geoJsonData = await response.json();
+        initializeMap();
+
+        if (mapRef.current) {
+          L.geoJSON(geoJsonData, {
+            style: function (feature) {
+              return { color: "white", weight: 1, fillColor: "white", fillOpacity: 0.4 };
+            },
+            onEachFeature: function (feature, layer: GeoJSON) {
+              layer.on('click', function (e: LeafletMouseEvent) {
+                const currentStyle = (layer.options as any).fillColor;
+                if (currentStyle === "white") {
+                  layer.setStyle({ fillColor: "#2ABF7A", fillOpacity: 0.6 });
+                } else {
+                  layer.setStyle({ fillColor: "white", fillOpacity: 0.4 });
+                }
+              });
+            }
+          }).addTo(mapRef.current);
+        }
       } catch (error) {
         console.error("Error fetching GeoJSON data:", error);
       }
     };
 
     fetchData();
+
+    // Cleanup function to remove the map on component unmount
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
   }, []);
 
   return (
-    <div id="map" style={{ height: '550px', width: '827px' ,borderRadius: '15px' }}>
-      <MapContainer
-        center={[30.505, -0.09]}
-        zoom={2}
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
+    <div id="map" style={{ height: '550px', width: '827px', borderRadius: '15px' }}></div>
   );
 };
 
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,24 +1,28 @@
-// pages/index.tsx
+// src/pages/map.tsx
 "use client";
 
 import React from 'react';
-import CountryShapes from '../api/CountryShapes';
-import WorldMap from '../../components/WorldMap';
+import dynamic from 'next/dynamic';
+import CountryShapes from '../api/CountryShapes'; // Assurez-vous que ce chemin est correct
 
+// Charger WorldMap dynamiquement côté client
+const DynamicWorldMap = dynamic(() => import('../../components/WorldMap'), {
+  ssr: false
+});
 
 const Home: React.FC = () => {
   return (
     <div>
       <div className='w-full flex justify-center items-center my-8'>
-        <h1>Voyager autours du monde</h1>
+        <h1>Voyager autour du monde</h1>
       </div>
       <div className='w-full flex justify-center items-center'>
         <div className='w-full flex gap-8'>
           <div className='bg-secondary w-1/4 rounded-[15px]'>
-              test
+            test
           </div>
           <div className="w-full max-w-1/3">
-            <WorldMap />
+            <DynamicWorldMap />
           </div>
         </div>
       </div>
