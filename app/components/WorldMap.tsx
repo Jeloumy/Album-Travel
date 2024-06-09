d3cfb92f5bafb0df83aedd194797b92883e22ce8
Sélèna Boullée <sboullee@kalya-sante.com>
2024-06-09 22:16:59 +0200

    feat: add victory + play again

--- app/components/WorldMap.tsx
+++ app/components/WorldMap.tsx
@@ -1,13 +1,22 @@
-// src/components/WorldMap.tsx
 import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
-import L, { GeoJSON, LeafletMouseEvent } from 'leaflet';
+import L, { GeoJSON } from 'leaflet';
 import axios from 'axios';
-import { Country } from '../../app/types/country';
+import { Country } from '../types/country';
 
+interface WorldMapProps {
+  setCountryClicked: (country: any) => void;
+  setActivePlay: (active: boolean) => void;
+  secretCountry: Country | null;
+  activePlay: boolean;
+  numberOfClicks: number;
+  setNumberOfClicks: (number: number) => void;
+}
 
-const WorldMap: React.FC<{ setCountryClicked: (country: any) => void; secretCountry: Country | null }> = ({ setCountryClicked, secretCountry }) => {
+const WorldMap: React.FC<WorldMapProps> = ({ setCountryClicked, secretCountry, activePlay, setActivePlay, numberOfClicks, setNumberOfClicks }) => {
   const mapRef = useRef<L.Map | null>(null);
+  let lastCountryClicked = "";
+  let prevCount = 0;
 
   useEffect(() => {
     const initializeMap = () => {
@@ -34,28 +43,33 @@ const WorldMap: React.FC<{ setCountryClicked: (country: any) => void; secretCoun
               return { color: "#cccccc", weight: 1, fillColor: "#ffffff", fillOpacity: 0.4 };
             },
             onEachFeature: function (feature, layer: GeoJSON) {
-              layer.on('click', async function (e) { // Marking function as async
-                const currentStyle = (layer.options as any).fillColor;
+              layer.on('click', async function () {
+                if (!activePlay) return; // Ignore clicks if the game is not active
+
                 console.log("Clicked on", feature.properties.name);
-
-                if (feature.properties.name !== secretCountry?.name) {
-                  layer.setStyle({ fillColor: "#FF7D5C", fillOpacity: 0.6 });
-                } else {
-                  layer.setStyle({ fillColor: "#9FE88D", fillOpacity: 0.4 });
-                }
-
-                try {
-                  await axios.get(`https://countryapi.io/api/name/${feature.properties.name}?apikey=LDs6saC8be44btzKGNvgqxGDwkqWBcXSlzBva4DR`).then(
-                    (countryResponse) => {
-                      const countryKey = Object.keys(countryResponse.data)[0];
-                      console.log(countryResponse.data[countryKey]);
-                      setCountryClicked(countryResponse.data[countryKey]);
-                    }).catch(error => {
-                      console.error("Error fetching country data:", error);
-                    }
-                  );
-                } catch (error) {
-                  console.error("Error fetching country data:", error);
+                if (lastCountryClicked !== feature.properties.name) {
+                  lastCountryClicked = feature.properties.name;
+                  prevCount ++;
+                  setNumberOfClicks(prevCount);
+                  if (feature.properties.name !== secretCountry?.name) {
+                    layer.setStyle({ fillColor: "#FF7D5C", fillOpacity: 0.6 });
+                  } else {
+                    layer.setStyle({ fillColor: "#9FE88D", fillOpacity: 0.4 });
+                    setActivePlay(false);
+                  }
+                  try {
+                    await axios.get(`https://countryapi.io/api/name/${feature.properties.name}?apikey=${process.env.NEXT_PUBLIC_API_KEY}`).then(
+                      (countryResponse) => {
+                        const countryKey = Object.keys(countryResponse.data)[0];
+                        console.log(countryResponse.data[countryKey]);
+                        setCountryClicked(countryResponse.data[countryKey]);
+                      }).catch(error => {
+                        console.error("Error fetching country data:", error);
+                      }
+                    );
+                  } catch (error) {
+                    console.error("Error fetching country data:", error);
+                  }
                 }
               });
             }
@@ -75,7 +89,7 @@ const WorldMap: React.FC<{ setCountryClicked: (country: any) => void; secretCoun
         mapRef.current = null;
       }
     };
-  }, [setCountryClicked, secretCountry]);
+  }, [setCountryClicked, secretCountry, activePlay, setActivePlay, setNumberOfClicks]); // setNumberOfClicks est utilisé dans l'effet
 
   return (
     <div id="map" style={{ height: '550px', width: '700px', borderRadius: '15px' }}></div>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 // /app/home/page.tsx
 "use client";
 
-import React, { useState,useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import AnswerCube from '../components/answer-cube';
 import { Country } from '../types/country';
@@ -12,71 +12,84 @@ const DynamicWorldMap = dynamic(() => import('../components/WorldMap'), {
 });
 
 const Home: React.FC = () => {
-  const initialized = useRef(false)
+  const initialized = useRef(false);
   const [countryClicked, setCountryClicked] = useState<Country | null>(null);
   const [secretCountry, setSecretCountry] = useState<Country | null>(null);
+  const [activePlay, setActivePlay] = useState<boolean>(false);
+  const [numberOfClicks, setNumberOfClicks] = useState<number>(0);
 
   useEffect(() => {
     if (!initialized.current) {
       initialized.current = true;
-      const fetchRandomCountry = async () => {
-        let randomCountry;
-        try {
-          const response = await fetch("https://countryapi.io/api/all?apikey=LDs6saC8be44btzKGNvgqxGDwkqWBcXSlzBva4DR");
-          const data = await response.json();
-          let population = 0;
-          while (population < 100000){
-            const countries = Object.keys(data);
-            const randomIndex = Math.floor(Math.random() * countries.length);
-            const randomCountryCode = countries[randomIndex];
-            randomCountry = data[randomCountryCode];
-            population = randomCountry.population;
-          }
-          setSecretCountry(randomCountry);
-          console.log('Random country population:', randomCountry.population);
-        } catch (error) {
-          console.error("Error fetching random country:", error);
-        }
-      };
-      
-    fetchRandomCountry();}
+      initializedTheGame();
+    }
   }, []);
+
+  async function initializedTheGame() {
+    setNumberOfClicks(0);
+    setActivePlay(true);
+    let randomCountry;
+    try {
+      const response = await fetch(`https://countryapi.io/api/all?apikey=${process.env.NEXT_PUBLIC_API_KEY}`);
+      const data = await response.json();
+      let population = 0;
+      while (population < 100000) {
+        const countries = Object.keys(data);
+        const randomIndex = Math.floor(Math.random() * countries.length);
+        const randomCountryCode = countries[randomIndex];
+        randomCountry = data[randomCountryCode];
+        population = randomCountry.population;
+      }
+      setSecretCountry(randomCountry);
+      setCountryClicked(null);
+      console.log('Random country population:', randomCountry.name);
+    } catch (error) {
+      console.error("Error fetching random country:", error);
+    }
+  }
 
   return (
     <div>
       <HeaderNav/>
-      <div className='w-full flex justify-center items-center my-8'>
+      {/* <div className='w-full flex justify-center items-center my-8'>
         <h1>Voyager autour du monde</h1>
-      </div>
+      </div> */}
       <div className='w-full flex gap-8'>
         <div className='bg-neutral w-3/4 rounded-[15px] p-6 flex flex-col gap-4'>
           <div className="p-4 border-2 border-neutral-content rounded">
             <h2>Indicateurs de couleur</h2>
-            <div className="flex gap justify-between mt-4">
-              <AnswerCube label="Correct" colorClass="bg-primary" info="e" dataCountrySecret=""/>
-              <AnswerCube label="Incorrect" colorClass="bg-secondary" info="e" dataCountrySecret=""/>
-              <AnswerCube label="Plus grand" colorClass="bg-accent" info="e" dataCountrySecret=""/>
-              <AnswerCube label="Plus petit" colorClass="bg-info" info="e" dataCountrySecret=""/>
+            <div className="flex gap justify-around mt-4">
+              <AnswerCube label="Correct" colorClass="bg-primary" info="" dataCountrySecret="" />
+              <AnswerCube label="Incorrect" colorClass="bg-secondary" info="" dataCountrySecret="" />
+              <AnswerCube label="Plus grand" colorClass="bg-accent" info="" dataCountrySecret="" />
+              <AnswerCube label="Plus petit" colorClass="bg-info" info="" dataCountrySecret="" />
             </div>
           </div>
           <div className="p-4 border-2 border-neutral-content rounded">
             <h2>Résultat</h2>
             {countryClicked && secretCountry && (
-              <div className="flex gap justify-between mt-4">
-                  <AnswerCube label="Superficie" colorClass="" info={countryClicked.area} dataCountrySecret={secretCountry.area} />
-                  <AnswerCube label="Continent" colorClass="" info={countryClicked.region} dataCountrySecret={secretCountry.region}/>
-                  <AnswerCube label="Région" colorClass="" info={countryClicked.subregion} dataCountrySecret={secretCountry.subregion}/>
-                  <AnswerCube label="Population" colorClass="" info={countryClicked.population} dataCountrySecret={secretCountry.population}/>
+              <div className="flex gap justify-around mt-4">
+                <AnswerCube label="Superficie" colorClass="" info={countryClicked.area} dataCountrySecret={secretCountry.area} />
+                <AnswerCube label="Continent" colorClass="" info={countryClicked.region} dataCountrySecret={secretCountry.region} />
+                <AnswerCube label="Région" colorClass="" info={countryClicked.subregion} dataCountrySecret={secretCountry.subregion} />
+                <AnswerCube label="Population" colorClass="" info={countryClicked.population} dataCountrySecret={secretCountry.population} />
               </div>
-            )
-            }
+            )}
+            {!activePlay && secretCountry &&(
+              <div className="flex gap-4 mt-4">
+                <div>
+                  <h1>Victoire !</h1>
+                  <p>Le pays a deviné était bien {secretCountry.name}; Vous avez réussi en {numberOfClicks} essais</p>
+                </div>
+                <button className="bg-primary text-neutral py-2 px-4 rounded" onClick={initializedTheGame}>Rejouer</button>
+              </div>
+            )}
           </div>
         </div>
         <div className="w-full max-w-1/2">
-        <DynamicWorldMap setCountryClicked={setCountryClicked} secretCountry={secretCountry} />
+          <DynamicWorldMap setCountryClicked={setCountryClicked} secretCountry={secretCountry} activePlay={activePlay} setActivePlay={setActivePlay} numberOfClicks={numberOfClicks} setNumberOfClicks={setNumberOfClicks}/>
         </div>
       </div>
-
     </div>
   );
 };
