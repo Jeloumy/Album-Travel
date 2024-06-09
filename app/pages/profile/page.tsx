106349c093510fb29e74d83e0e809526c50a1088
Jeloumy <jeremy60.duflot60@gmail.com>
2024-06-09 22:45:38 +0200

    fix : deploy vercel (profil page comment)

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -1,35 +1,40 @@
-// // app/pages/profile/page.tsx
-// "use client"; // Indique que ce module est du code côté client
+"use client"; // Indique que ce module est du code côté client
 
-// import { useSession, signIn, signOut } from 'next-auth/react';
-// import { useEffect } from 'react';
-// import { useRouter } from 'next/navigation';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-// const ProfilePage = () => {
-//   const { data: session, status } = useSession();
-//   const router = useRouter();
+const ProfilePage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-//   useEffect(() => {
-//     if (status === 'unauthenticated') {
-//       router.push('/api/auth/signin');
-//     }
-//   }, [status, router]); // Ajout de router aux dépendances
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push('/api/auth/signin');
+    }
+  }, [status, router]); // Ajout de router aux dépendances
 
-//   if (status === 'loading') {
-//     return <div>Loading...</div>;
-//   }
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
 
-//   if (status === 'unauthenticated') {
-//     return <div>Redirecting...</div>;
-//   }
+  if (status === 'unauthenticated') {
+    return <div>Redirecting...</div>;
+  }
 
-//   return (
-//     <div>
-//       <h1>Profile</h1>
-//       <pre>{JSON.stringify(session, null, 2)}</pre>
-//       <button onClick={() => signOut()}>Sign out</button>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Profile</h1>
+      {session ? (
+        <>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <div>No session data available</div>
+      )}
+    </div>
+  );
+};
 
-// export default ProfilePage;
+export default ProfilePage; // Assurez-vous qu'il y a un export par défaut
