a40706df93a92da1d060beca5a6939e1267c19d9
Jeloumy <jeremy60.duflot60@gmail.com>
2024-06-18 01:06:58 +0200

    fix : middleware with cookies

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const secret = process.env.AUTH_SECRET;
-
-  if (!secret) {
-    throw new Error('AUTH_SECRET environment variable is not defined');
-  }
-
-  const token = await getToken({ req, secret });
+  console.log('Checking session token...');
+  const sessionCookie = req.cookies.get('authjs.session-token') || req.cookies.get('__Secure-next-auth.session-token');
 
   const isAuthPage = req.nextUrl.pathname === '/login';
   const isProtectedPage = !['/login', '/register', '/api/auth', '/api/register'].includes(req.nextUrl.pathname);
 
-  console.log(`Token: ${token}`);
+  console.log(`Session Cookie: ${sessionCookie}`);
   console.log(`isAuthPage: ${isAuthPage}`);
   console.log(`isProtectedPage: ${isProtectedPage}`);
 
-  if (isAuthPage && token) {
+  if (isAuthPage && sessionCookie) {
     return NextResponse.redirect(new URL('/home', req.url));
   }
 
-  if (isProtectedPage && !token) {
+  if (isProtectedPage && !sessionCookie) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
